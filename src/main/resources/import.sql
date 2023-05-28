41c08b75317e7332085532a02c4c63d5bd6b097a
lemuel-sousa <lemuel124.ldsp@gmail.com>
2023-05-28 12:58:35 -0300

    feat: add seed database

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+-- Inserting "foods"
+INSERT INTO foods (title, image_url, price) VALUES ('Sanduíce', 'https://t2.uc.ltmcdn.com/pt/posts/0/1/3/como_fazer_sanduiche_natural_9310_orig.jpg', 10);
+INSERT INTO foods (title, image_url, price) VALUES ('Pizza', 'https://img.freepik.com/fotos-gratis/pizza-havaiana_74190-2500.jpg', 60);
+INSERT INTO foods (title, image_url, price) VALUES ('Coxinhas', 'https://i.pinimg.com/originals/89/b3/e0/89b3e07ec476f29ba6cd641887c67b0b.png', 15);
