ea4adccc8742bd74f359bd385934587b43b03f2d
lemuel-sousa <lemuel124.ldsp@gmail.com>
2023-05-31 15:23:12 -0300

    feat: update seed database

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,10 @@
 -- Inserting "foods"
-INSERT INTO foods (title, image_url, price) VALUES ('Sanduíce', 'https://t2.uc.ltmcdn.com/pt/posts/0/1/3/como_fazer_sanduiche_natural_9310_orig.jpg', 10);
-INSERT INTO foods (title, image_url, price) VALUES ('Pizza', 'https://img.freepik.com/fotos-gratis/pizza-havaiana_74190-2500.jpg', 60);
-INSERT INTO foods (title, image_url, price) VALUES ('Coxinhas', 'https://i.pinimg.com/originals/89/b3/e0/89b3e07ec476f29ba6cd641887c67b0b.png', 15);
+INSERT INTO foods (title, image, price) VALUES ('Costela bovina', 'https://img.freepik.com/fotos-gratis/comida-mexicana-ainda-vida_23-2148131302.jpg', 70);
+INSERT INTO foods (title, image, price) VALUES ('Croissants caramelizado', 'https://img.freepik.com/fotos-gratis/croissants-em-cima-da-mesa_144627-21534.jpg', 18);
+INSERT INTO foods (title, image, price) VALUES ('Espetão Misto', 'https://img.freepik.com/fotos-premium/um-prato-de-carne-no-palito_871710-1386.jpg', 30);
+INSERT INTO foods (title, image, price) VALUES ('Sorvete de Chocolate', 'https://img.freepik.com/fotos-gratis/sorvete-de-chocolate_144627-19458.jpg', 18);
+INSERT INTO foods (title, image, price) VALUES ('Hamburguer', 'https://img.freepik.com/fotos-gratis/hamburguer-saboroso-isolado-no-fundo-branco-fastfood-de-hamburguer-fresco-com-carne-e-queijo_90220-1063.jpg', 32);
+INSERT INTO foods (title, image, price) VALUES ('Bife Grelhado com purê de batata', 'https://img.freepik.com/fotos-premium/um-prato-de-bife-e-molho-com-uma-pequena-tigela-de-manteiga_521733-339.jpg', 60);
+INSERT INTO foods (title, image, price) VALUES ('Limonada com Hortelã', 'https://img.freepik.com/fotos-premium/copo-de-bebida-de-limao-com-hortela-fresca-de-limoes-e-cubos-de-gelo-em-branco_259229-1113.jpg', 26);
+INSERT INTO foods (title, image, price) VALUES ('Gobhi seco', 'https://img.freepik.com/fotos-premium/gobhi-seco-manchurian_57665-7242.jpg', 80);
+INSERT INTO foods (title, image, price) VALUES ('Cosmopolitan Cocktail', 'https://img.freepik.com/fotos-gratis/copo-de-bebida-cocktail-com-uma-fatia-de-limao-contra-um-fundo-rosa_23-2148102098.jpg', 33);
