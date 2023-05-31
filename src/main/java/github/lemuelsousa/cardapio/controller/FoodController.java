97643cacf6d349361033c296954893e8ed2a4b05
lemuel-sousa <lemuel124.ldsp@gmail.com>
2023-05-31 15:22:53 -0300

    feat: refactor post food

--- src/main/java/github/lemuelsousa/cardapio/controller/FoodController.java
+++ src/main/java/github/lemuelsousa/cardapio/controller/FoodController.java
@@ -3,29 +3,37 @@ package github.lemuelsousa.cardapio.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import github.lemuelsousa.cardapio.DTO.FoodRequestDTO;
 import github.lemuelsousa.cardapio.DTO.FoodResponseDTO;
 import github.lemuelsousa.cardapio.service.FoodService;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/food")
+@Slf4j
 public class FoodController {
 
     @Autowired
     private FoodService foodService;
 
-    @PostMapping
-    public void createFood(@RequestBody FoodRequestDTO food){
+    @PostMapping    
+    public void createFood(@RequestBody FoodRequestDTO data){
+        foodService.saveFood(data);
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<FoodResponseDTO> getAll(){
+
+        log.info("Listing All: [{}]", foodService.findAll());
         return foodService.findAll();
     }
 }
