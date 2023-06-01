26855a8ea3161cfdd23864b836ca6189a548c6a9
lemuel-sousa <lemuel124.ldsp@gmail.com>
2023-06-01 13:34:39 -0300

    feat: change service class to interface

--- src/main/java/github/lemuelsousa/cardapio/service/FoodService.java
+++ src/main/java/github/lemuelsousa/cardapio/service/FoodService.java
@@ -2,29 +2,13 @@ package github.lemuelsousa.cardapio.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import github.lemuelsousa.cardapio.DTO.FoodRequestDTO;
 import github.lemuelsousa.cardapio.DTO.FoodResponseDTO;
-import github.lemuelsousa.cardapio.entity.Food;
-import github.lemuelsousa.cardapio.repository.FoodRepository;
 
-@Service
-public class FoodService {
+public interface FoodService {
     
-    @Autowired
-    private FoodRepository foodRepository;
+    List<FoodResponseDTO> findAll();
 
-
-    public void saveFood(FoodRequestDTO data){
-       foodRepository.save(new Food(data));
-    }
-
-    public List<FoodResponseDTO> findAll(){
-        return foodRepository.findAll().stream().map(FoodResponseDTO::new).toList();
-    }
-
-
+    FoodResponseDTO register(FoodRequestDTO foodDTO);
 
 }
