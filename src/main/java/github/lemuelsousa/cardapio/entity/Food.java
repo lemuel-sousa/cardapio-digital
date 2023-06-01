f4121a740ab8281b3e27f0e762d949e3466ed543
lemuel-sousa <lemuel124.ldsp@gmail.com>
2023-06-01 13:33:05 -0300

    feat: add Builder constructor for Food entity

--- src/main/java/github/lemuelsousa/cardapio/entity/Food.java
+++ src/main/java/github/lemuelsousa/cardapio/entity/Food.java
@@ -1,36 +1,35 @@
 package github.lemuelsousa.cardapio.entity;
 
-import github.lemuelsousa.cardapio.DTO.FoodRequestDTO;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity( name = "foods")
-@Table( name = "foods")
+@Entity @Table( name = "foods")
 @NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
+@Getter @Setter
 public class Food {
     
     @Id @GeneratedValue( strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
     private Long id;
     private String title;
     @Column(columnDefinition = "TEXT")
     private String image;
     private Integer price;
 
-    public Food( FoodRequestDTO data){
-        this.title = data.title();
-        this.image = data.image();
-        this.price = data.price();
+    @Builder
+    public Food(String title, String image, Integer price){
+        this.title = title;
+        this.image = image;
+        this.price = price;
     }
 
 }
