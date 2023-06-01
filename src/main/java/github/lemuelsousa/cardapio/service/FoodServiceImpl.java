8ddde0db355326369c62be7dee2a5ffd0974c474
lemuel-sousa <lemuel124.ldsp@gmail.com>
2023-06-01 13:35:09 -0300

    feat: add FoodServiceImpl

---
+++ src/main/java/github/lemuelsousa/cardapio/service/FoodServiceImpl.java
@@ -0,0 +1,37 @@
+package github.lemuelsousa.cardapio.service;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import github.lemuelsousa.cardapio.DTO.FoodRequestDTO;
+import github.lemuelsousa.cardapio.DTO.FoodResponseDTO;
+import github.lemuelsousa.cardapio.entity.Food;
+import github.lemuelsousa.cardapio.repository.FoodRepository;
+import github.lemuelsousa.cardapio.util.FoodMapper;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class FoodServiceImpl implements FoodService {
+    
+    private final FoodRepository foodRepository;
+
+    private final FoodMapper foodMapper;
+
+    @Override
+    public FoodResponseDTO register(FoodRequestDTO foodDTO){
+
+        Food newFood = foodMapper.toFood(foodDTO);
+
+        return foodMapper.toFoodDTO(foodRepository.save(newFood));
+    }
+
+    @Override
+    public List<FoodResponseDTO> findAll(){
+        return foodMapper.toFoodsDTO(foodRepository.findAll());
+    }
+
+}
